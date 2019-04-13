5d75f6abe6bc970983c8343e308252dd6b93ff9e
zhai <zhai89@qq.com>
2019-04-13 17:13:07 +0800

    add ex1 and ex2

---
+++ hardway/ex1.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+# 如果输入中文，需要加入上面一行，中间用=或者:均可
+# 上面一行必须位于第一行，且#号不可省去
+# 也可以使用如下的标记，功能是一样的，都要放在第一行
+# -*- coding: utf-8 -*-
+print("Hello World!")
+print("Hello Again")
+print("你好")
---
+++ hardway/ex2.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+# this is a comment
+# this is another comment
+print("comment exercise")
+
+'''
+multiline comment
+多行注释
+多行注释
+'''
+print("多行注释")
+
+"""
+multiline comment
+多行注释
+多行注释
+"""
+print("多行注释2")
+
+print("行尾注释") # comment at line end
