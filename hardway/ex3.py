4952cb5cf1854c5e87c6870ce51560c124314722
zhai <zhai89@qq.com>
2019-04-14 09:31:43 +0800

    add ex3 4 5 6 7 8 9

---
+++ hardway/ex3.py
@@ -0,0 +1,28 @@
+# coding:utf-8
+
+# + - * /
+print("Hens", 25 + 30 / 6)
+print("Rooster", 100 - 25 * 3 % 4)
+
+# %
+print("25 / 3 = ", 25 / 3)
+print("25 % 3 = ", 25 % 3)
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# > < >= <=
+print("Is it true that 3 + 2 < 5 - 7?")
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+
+# float /
+print("float divide", 1 / 3)
+print("float divide", 1.0 / 3)
+print("float divide", 1 / 3.0)
